d96117b5fa0260a1258904d59eb275e234682f9d
Edom1yas <edomiyashayle20@gmail.com.noreply.github.com>
2025-07-15 16:19:11 -0500

    Impliment essential serversided CRUD
     - Implement Get method
     - Implement Delete method
     - Implement update/put method

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -26,6 +27,46 @@ app.post("/api/products", async (req, res) => {
     }
 });
 
+app.delete("/api/products/:id", async (req,res) => {
+    const {id} = req.params;
+    console.log("id:", id);
+    
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message:"Product successfuly deleted"})
+    }catch(error){
+        console.log("Error deleting product", error.message);
+        res.status(404).json ({success:false, message:"Product not found"})
+    }
+});
+
+app.get("/api/products", async(req,res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json ({success:true, data:products});
+    }catch(error){
+        console.log("Error fetching products", error.messasge);
+        res.status(500).json({success:false, message:"Server Error"});
+    };
+});
+
+app.put("/api/products/:id", async(req,res) => {
+    const {id} = req.params;
+    console.log("id:", id);
+    const product = req.body;
+
+    // if(!mongoose.Types.ObjectId.isValid(id)){
+    //     return res.status(404).json({success:false, message:"Invalid product ID"});
+    // }
+
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success:true, data:updatedProduct});
+    }catch(error){
+        console.log("Error Updating producet", error.message)
+        res.status(500).json({success:false, message:"Server Error"});
+    }
+});
 
 app.listen(5000, () => {
     connectDB();
